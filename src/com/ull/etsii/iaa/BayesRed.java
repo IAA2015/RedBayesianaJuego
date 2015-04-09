befd3f8b99ee8830cfa7a1c92bb32f96a40d7906
wyllman <wyllman@gmail.com>
2015-04-09 16:20:46 +0100

    Estructura inicial

---
+++ src/com/ull/etsii/iaa/BayesRed.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ull.etsii.iaa;
+
+/**
+ * @author wyllman
+ *
+ */
+public class BayesRed {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
