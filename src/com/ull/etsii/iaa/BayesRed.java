12c23f738850958c2537fd92514276d5b2769162
wyllman <wyllman@gmail.com>
2015-04-09 16:29:15 +0100

    Enums base creados.

--- src/com/ull/etsii/iaa/BayesRed.java
+++ src/com/ull/etsii/iaa/BayesRed.java
@@ -4,10 +4,29 @@
 package com.ull.etsii.iaa;
 
 /**
- * @author wyllman
+ * @author
  *
  */
 public class BayesRed {
+	
+	public enum Acciones { 
+	   Atacar, BuscarArmas, BuscarEnergia, Explorar, Huir, DetectarPeligro
+	}
+	
+	public enum VidaType {
+		Alto, Bajo
+	}
+	
+	public enum ArmaType {
+		Armado, Desarmado
+	}
+	
+	public enum EnemigosType {
+		Muchos, Pocos
+	}
+	
+	
+	
 
 	/**
 	 * @param args
